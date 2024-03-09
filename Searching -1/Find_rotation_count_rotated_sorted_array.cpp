d893b29332fe8645e895d8e6ef710254a8b10c3d
Abhishek Singh <abhishek27033@gmail.com>
2024-03-09 11:20:22 +0530

    Added some modific

--- Searching -1/Find_rotation_count_rotated_sorted_array.cpp
+++ Searching -1/Find_rotation_count_rotated_sorted_array.cpp
@@ -1,17 +1,33 @@
-class Solution{
+class Solution {
 public:
-int findKRotation(int arr[], int n) {
-        int low=0;
-        int high=n-1;
-        while (low < high)
+    int findKRotation(int arr[], int n) {
+        int low = 0;
+        int high = n - 1;
+        int ans = INT_MAX, idx = -1;
+        while (low <= high)
         {
             int mid = low + (high - low) / 2;
-            if (arr[mid] > arr[high])
+            // cout<<mid<<endl;
+            if (arr[low] <= arr[mid])
+            {
+                if (ans > arr[low])
+                {
+                    ans = arr[low];
+                    idx = low;
+                }
                 low = mid + 1;
+            }
             else
-                high = mid;
+            {
+                if (ans > arr[mid])
+                {
+                    ans = arr[mid];
+                    idx = mid;
+                }
+                high = mid - 1;
+            }
         }
-        return low%n;
-        
+        return idx;
+
     }
 };

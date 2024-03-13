20c1543921f2858b602f04f64a24e52bbf02e6f9
Abhishek Singh <abhishek27033@gmail.com>
2024-03-13 22:59:38 +0530

    Initial Commit

--- Searching -3/Maximum_Lunch_Box.cpp
+++ Searching -3/Maximum_Lunch_Box.cpp
@@ -1,27 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+unordered_map<int, int> mp;
+bool ispossible(int m, int a)
+{
+	int c = 0;
+	for (auto it : mp)
+	{
+		int f = (it.second / m);
+		c += f;
+		if (c >= a)
+			return true;
+	}
+	return false;
+}
 int main() {
 	// your code goes here
 	int n;
 	cin >> n;
 	vector<int> b(n);
-	unordered_map<int, int> mp;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> b[i];
 		mp[b[i]]++;
 	}
-	vector<int> fr;
-	for (auto it : mp)
-		fr.push_back(it.second);
-	sort(fr.rbegin(), fr.rend());
 	int a;
 	cin >> a;
-	int l = 1, h = fr[0], ans = -1;
+	int l = 1, h = b.size() / a, ans = -1;
 	while (l <= h)
 	{
 		int m = l + (h - l) / 2;
-		if (fr.size() > a - 1 && fr[a - 1] >= m)
+		if (ispossible(m, a))
 		{
 			ans = m;
 			l = m + 1;
